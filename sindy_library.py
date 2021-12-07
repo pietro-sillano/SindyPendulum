e8a3cebdf7b3c03a3ea0ce502d0d18b61aac2987
Pietro Sillano <pietrosillano@gmail.com>
2021-12-07 23:10:44 +0100

    Update sindy_library.py

--- sindy_library.py
+++ sindy_library.py
@@ -139,13 +139,7 @@ class SINDyLibrary():
 
 
     def transform(self, z):
-      #theta = [cand_func(z) for cand_func in self.candidate_functions]
-      temp = []
-      for cand_func in self.candidate_functions:
-        theta = cand_func(z)
-        print('z',z.shape)
-        print('theta',theta.shape)
-        temp.append(theta)
+      theta = [cand_func(z) for cand_func in self.candidate_functions]
       out =  torch.cat(theta, axis=1)
       return out
 
