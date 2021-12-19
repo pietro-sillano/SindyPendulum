3557a2a00fa3012303111c4bd6e644974992cdff
pietro-sillano <pietrosillano@gmail.com>
2021-12-19 23:18:33 +0100

    changing dt and tb

--- data_pendulum_image.py
+++ data_pendulum_image.py
@@ -79,8 +79,10 @@ if __name__ == "__main__":
     #COSTANTI e PARAMETRI
     #n_ics = 100
     ta = 0.
-    tb = 10.
-    dt = 0.1 
+    tb = 5.
+    dt = 0.02 
+    # tb = 10
+    # dt = 0.1
     NX = 51
     NY = 51
 
